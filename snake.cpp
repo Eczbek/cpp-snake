aa057cb95738a9cbe3f9865e987906b89e057199
Eczbek <eczbek.void@gmail.com>
2022-09-23 10:45:17 -0400

    Remove global variables

--- snake.cpp
+++ snake.cpp
@@ -15,20 +15,21 @@ struct Position {
 	int y;
 };
 
-constexpr Position size { 20, 20 };
-
-std::mt19937 rng(std::random_device {}());
-std::uniform_int_distribution distX(0, size.x - 1);
-std::uniform_int_distribution distY(0, size.y - 1);
-
-Position apple { distX(rng), distY(rng) };
-
-std::deque<Position> body { { distX(rng), distY(rng) } };
-Position direction { 0, 0 };
-
-int score = 0;
-
 int main() {
+	constexpr Position size { 20, 20 };
+
+	std::mt19937 rng(std::random_device {}());
+	std::uniform_int_distribution distX(0, size.x - 1);
+	std::uniform_int_distribution distY(0, size.y - 1);
+
+	Position apple { distX(rng), distY(rng) };
+
+	std::deque<Position> body { { distX(rng), distY(rng) } };
+	Position direction { 0, 0 };
+
+	int score = 0;
+	bool running = true;
+	
 	termios cooked;
 	tcgetattr(STDIN_FILENO, &cooked);
 	termios raw = cooked;
@@ -38,7 +39,7 @@ int main() {
 	fcntl(STDIN_FILENO, F_SETFL, blocking | O_NONBLOCK);
 	std::cout << "\033[?25l";
 
-	while (true) {
+	while (running) {
 		const Position head { (body[0].x + direction.x + size.x) % size.x, (body[0].y + direction.y + size.y) % size.y };
 
 		if (head.x == apple.x && head.y == apple.y) {
@@ -49,7 +50,7 @@ int main() {
 		if (std::find_if(body.begin() + 1, body.end(), [&](const Position& part) {
 			return part.x == head.x && part.y == head.y;
 		}) != body.end())
-			goto Reset;
+			break;
 		body.push_front(head);
 
 		std::string display;
@@ -64,10 +65,11 @@ int main() {
 
 		std::cout << "\033[2J\033[HScore: " << score << "\r\n" << display;
 		std::cout.flush();
+		
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		
 		char input = 0;
 		while (read(STDIN_FILENO, &input, 1) == 1);
-
 		switch (input) {
 			case 'd':
 				if (!direction.x)
@@ -86,11 +88,10 @@ int main() {
 					direction = { 0, -1 };
 				break;
 			case 'q':
-				goto Reset;
+				running = false;
 		}
 	}
 
-Reset:
 	std::cout << "\033[?25h";
 	fcntl(STDIN_FILENO, F_SETFL, blocking);
 	tcsetattr(STDIN_FILENO, TCSANOW, &cooked);
