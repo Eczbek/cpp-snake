10b9e5ff9e35975a8b05f43c9612331e32c27e5b
Eczbek <eczbek.void@gmail.com>
2025-03-08 18:53:44 -0500

    Miscellaneous fixes

--- main.cpp
+++ main.cpp
@@ -31,8 +31,8 @@ int main() {
 	};
 
 	term.clear_screen();
-	for (int y = 0; y < game_size.y; ++y) {
-		for (int x = 0; x < game_size.x; ++x) {
+	for (int x = 0; x < game_size.x; ++x) {
+		for (int y = 0; y < game_size.y; ++y) {
 			set_color_at({ x, y }, 0x007FFF);
 		}
 	}
@@ -50,13 +50,14 @@ int main() {
 		};
 
 		xieite::vec2<int> apple = rand_pos();
-	
-		std::array<xieite::vec2<int>, static_cast<std::size_t>(game_size.x * game_size.y)> snake_body;
+
 		xieite::vec2<int> snake_head = rand_pos();
+		xieite::vec2<int> snake_direction;
+	
+		std::array<xieite::vec2<int>, static_cast<std::size_t>(game_size.x * game_size.y)> snake_body = { snake_head };
 		std::size_t snake_start = 0;
 		std::size_t snake_end = 0;
 		std::size_t score = 0;
-		xieite::vec2<int> snake_direction;
 	
 		while (true) {
 			term.reset_style();
@@ -67,7 +68,9 @@ int main() {
 			}
 	
 			set_color_at(snake_head, 0x00FF00);
-			snake_body[++snake_start %= snake_body.size()] = snake_head = (snake_head + snake_direction + game_size) % game_size;
+			snake_head = (snake_head + snake_direction + game_size) % game_size;
+			++snake_start %= snake_body.size();
+			snake_body[snake_start] = snake_head;
 			if (snake_head == apple) {
 				++score;
 				apple = rand_pos();
@@ -86,33 +89,37 @@ int main() {
 	
 			std::fflush(stdout);
 			std::this_thread::sleep_for(100ms);
-			switch (term.read_key()) {
-			case xieite::keys::Q:
-			case xieite::keys::q:
-				return false;
-			case xieite::keys::down:
-				if (!snake_direction.y || !score) {
-					snake_direction = { 0, 1 };
+			const bool can_move_x = !snake_direction.x || !score;
+			const bool can_move_y = !snake_direction.y || !score;
+			for (bool input = true; input;) {
+				switch (term.read_key()) {
+				case xieite::keys::Q:
+				case xieite::keys::q:
+					return false;
+				case xieite::keys::right:
+					if (can_move_x) {
+						snake_direction = { 1, 0 };
+					}
+					break;
+				case xieite::keys::left:
+					if (can_move_x) {
+						snake_direction = { -1, 0 };
+					}
+					break;
+				case xieite::keys::down:
+					if (can_move_y) {
+						snake_direction = { 0, 1 };
+					}
+					break;
+				case xieite::keys::up:
+					if (can_move_y) {
+						snake_direction = { 0, -1 };
+					}
+					break;
+				default:
+					input = false;
 				}
-				break;
-			case xieite::keys::up:
-				if (!snake_direction.y || !score) {
-					snake_direction = { 0, -1 };
-				}
-				break;
-			case xieite::keys::right:
-				if (!snake_direction.x || !score) {
-					snake_direction = { 1, 0 };
-				}
-				break;
-			case xieite::keys::left:
-				if (!snake_direction.x || !score) {
-					snake_direction = { -1, 0 };
-				}
-				break;
-			default:;
 			}
-			while (std::fgetc(term.in) > 0);
 		}
 	})();
 
@@ -124,8 +131,8 @@ int main() {
 	}
 	std::fputs("Press any key to exit", term.out);
 	std::fflush(term.out);
-	while (std::fgetc(term.in) > 0);
 	std::this_thread::sleep_for(500ms);
+	while (std::fgetc(term.in) > 0);
 	term.block(true);
 	std::fgetc(term.in);
 }
